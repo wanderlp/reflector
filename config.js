47308210601d2e204eaccb719cc2c9dc845bc237
Wanderson <lopez.wanderson@gmail.com>
2015-07-14 16:22:22 -0600

    Adaptacion
    Cambio en parametrizacion de la configuracion

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var conf = module.exports = {};
 
 conf.server = {};
-conf.server.port = 443;
+conf.server.port = 9999;
 
 conf.log = {};
 conf.log.level = "silly"; // silly, debug, verbose, info, warn, error
